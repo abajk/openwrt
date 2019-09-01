4e1e69d4739c14153db87b84ec7378f2a9b288d5
Aleksander Jan <olek2@wp.pl>
2019-09-01 16:01:32 +0200

    lantiq: add support for D-Link DWR-966
    Hardware:
    - SoC: Lantiq GRX 389-720
    - CPU: 2x MIPS 34Kc 600 MHz
    - RAM: 128 MiB 300 MHz
    - Flash: 128 MiB NAND
    - Ethernet: Built-in Gigabit Ethernet switch, 4 ports
    - WiFi 2.4G: Lantiq PSB 81312 b/g/n 2x2:2
    - WiFi 5G: Qualcomm Atheros QCA9880 a/n/ac 3x3:3
    - LTE: Cat6 Modem connected via ASM1042 PCI-E to USB 3.0 Hub
    - USB: 2x USB 2.0
    - LEDs: 5x RSSI, 4G, 3G, 2G, ANT, 5G, 2.4G, WPS, Internet, 2x PWR (Green and Red)
    - Buttons: 1x Reset, 1x ANT, 1x WPS
    
    Backup:
    - enable ssh on stock firmware
    - login via ssh:
     user: admin
     pass: admin
    - mount USB drive
    - make backup:
      $ nanddump --file /tmp/mounts/<directory>/dwr966.mtd1.nanddump /dev/mtd1
      $ nanddump --file /tmp/mounts/<directory>/dwr966.mtd2.nanddump /dev/mtd2
      $ nanddump --file /tmp/mounts/<directory>/dwr966.mtd4.nanddump /dev/mtd4
    
    Installation:
    - enable tftpserver on address 192.168.1.100 and connect ethernet cable to router
    - copy initramfs to main tftp server directory and rename file to uImage:
      $ cp openwrt-lantiq-xrx200-dlink_dwr-966-initramfs-kernel.bin /srv/tftp/uImage
    - connect via serial
    - power on
    - stop autoboot and enter:
      $ setenv loadaddr 0x84000000
      $ run net_ram
    - download squashfs image via scp:
      $ scp openwrt-lantiq-xrx200-dlink_dwr-966-squashfs-sysupgrade.bin root@192.168.1.1:/tmp
    - run sysupgrade
      $ sysupgrade /tmp/openwrt-lantiq-xrx200-dlink_dwr-966-squashfs-sysupgrade.bin
    [At this momet boot fail 6 times due Invalid CRC]
    - in GRX390 console enter (red_led is executed by u-boot if CRC is invalid):
      $ setenv red_led run flash_flash
      $ saveenv
    - power off and power on. It normaly boot from flash.
    
    Restore original firmware
    - connect and mount USB drive
    - install nand-utils
    - erase flash:
      $ flash_erase /dev/mtd1 0 0
      $ flash_erase /dev/mtd2 0 0
      $ flash_erase /dev/mtd4 0 0
    - write stock firmware:
      $ nandwrite -p /dev/mtd1 dwr966.mtd1.nanddump
      $ nandwrite -p /dev/mtd2 dwr966.mtd2.nanddump
      $ nandwrite -p /dev/mtd4 dwr966.mtd4.nanddump
    
    Known issues:
    - U-boot fails to boot linux due ivalid CRC checksum. Kernel is larger than 2MB (hardcoded value in u-boot) and this causes invalid CRC calculation.
    - PCI-E doesn't work (WiFi, LTE)
    
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

---
+++ target/linux/lantiq/image/xrx330.mk
@@ -0,0 +1,23 @@
+define Device/dsa-migration
+  DEVICE_COMPAT_VERSION := 1.1
+  DEVICE_COMPAT_MESSAGE := Config cannot be migrated from swconfig to DSA
+endef
+
+define Device/dlink_dwr-966
+  $(Device/NAND)
+  DEVICE_VENDOR := D-Link
+  DEVICE_MODEL := DWR-966
+  SOC := xrx330
+  DEVICE_PACKAGES := kmod-usb-dwc2 \
+	xrx300-phy11g-firmware \
+	-ltq-vdsl-vr9-vectoring-fw-installer \
+	-kmod-ltq-vdsl-vr9-mei \
+	-kmod-ltq-vdsl-vr9 \
+	-kmod-ltq-atm-vr9 \
+	-kmod-ltq-ptm-vr9 \
+	-ltq-vdsl-vr9-app \
+	-dsl-vrx200-firmware-xdsl-a \
+	-dsl-vrx200-firmware-xdsl-b-patch \
+	-ppp-mod-pppoa
+endef
+TARGET_DEVICES += dlink_dwr-966
