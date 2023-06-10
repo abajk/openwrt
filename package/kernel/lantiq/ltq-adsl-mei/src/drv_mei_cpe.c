04096c9c7e4bc5b9bfcf679643c256296944acd1
Aleksander Jan Bajkowski <olek2@wp.pl>
2023-06-10 13:07:26 +0200

    package: lantiq: remove obsolete kernel switches
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

--- package/kernel/lantiq/ltq-adsl-mei/src/drv_mei_cpe.c
+++ package/kernel/lantiq/ltq-adsl-mei/src/drv_mei_cpe.c
@@ -29,7 +29,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
-#include <linux/version.h>
 #include <generated/utsrelease.h>
 #include <linux/types.h>
 #include <linux/fs.h>
